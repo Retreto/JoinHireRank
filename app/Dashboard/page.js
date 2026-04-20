3b3bcef9d3da42e24fa3bdae5298d5746224209e
Retreto <noob.gamer.arnav22@gmail.com>
2026-04-21 01:40:38 +0530

    Add Dashboard component with candidate information

---
+++ app/Dashboard/page.js
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Dashboard(){return <main style={{padding:'30px'}}><h1>Candidate Dashboard</h1><p>Profile Strength: 82%</p><p>Applications: 5</p><p>Interviews: 1</p><Link href='/'>Home</Link></main>}
