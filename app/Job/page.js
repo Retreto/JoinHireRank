0112fc4adce8716d777916228cf6b136fc2e1ccb
Retreto <noob.gamer.arnav22@gmail.com>
2026-04-21 01:36:06 +0530

    Create Jobs page with job listings and navigation

---
+++ app/Job/page.js
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Jobs(){return <main style={{padding:'30px'}}><h1>Jobs</h1><ul><li>Frontend Developer</li><li>Backend Engineer</li><li>Data Analyst</li></ul><Link href='/'>Home</Link></main>}
