f901e15e9f8de8b08bc8932a29a5bdbe7258a86d
Retreto <noob.gamer.arnav22@gmail.com>
2026-04-21 01:39:42 +0530

    Add Signup page with form and navigation

---
+++ app/Sing up/page.js
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Signup(){return <main style={{padding:'30px'}}><h1>Create Account</h1><input placeholder='Full Name'/><br/><br/><input placeholder='Email'/><br/><br/><button>Join Free</button><br/><br/><Link href='/'>Home</Link></main>}
