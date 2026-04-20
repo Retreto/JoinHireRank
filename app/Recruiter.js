11f34c6504ca5e9e3042df21aca511392c453db0
Retreto <noob.gamer.arnav22@gmail.com>
2026-04-21 01:41:27 +0530

    Add Recruiter component with job posting features

---
+++ app/Recruiter.js
@@ -0,0 +1,2 @@
+import Link from 'next/link'
+export default function Recruiter(){return <main style={{padding:'30px'}}><h1>Recruiter Panel</h1><p>Post jobs, review candidates, shortlist talent.</p><Link href='/'>Home</Link></main>}
